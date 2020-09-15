ba14fdecc7821d066ba689887456a6d232353d91
Gerardo <gerodel98@gmail.com>
2020-09-14 21:36:37 -0500

    Added firebase sdk and successfully created an item in a collection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState } from "react";
+import { db } from "./firebase";
 
 function App() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [mail, setMail] = useState("");
+
+  const validateName = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  };
+
+  const validatePhone = (event) => {
+    setPhone(event.target.value);
+    console.log(phone);
+  };
+
+  const validateMail = (event) => {
+    setMail(event.target.value);
+    console.log(mail);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    db.collection('contacts').add({
+      name,
+      phone,
+      mail
+    })
+    .then(() => {
+      alert("Data added successfully")
+    })
+    .catch((error) => {
+      alert(error.message)  
+    })
+  }
+
   return (
     <div>
-      Hola mundo
+      <h1> Rifa navideña Miles Christi </h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nombre completo:
+          <br />
+          <input
+            placeholder="Nombre completo"
+            value={name}
+            onChange={(e) => validateName(e)}
+          />
+        </label>
+        <br />
+        <label>
+          Celular o fijo:
+          <br />
+          <input
+            placeholder="3320202020"
+            value={phone}
+            onChange={(e) => validatePhone(e)}
+          />
+        </label>
+        <br />
+        <label>
+          Correo electrónico:
+          <br />
+          <input
+            placeholder="nombre@mail.com"
+            value={mail}
+            onChange={(e) => validateMail(e)}
+          />
+        </label>
+        <br />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+let firebaseApp = firebase.initializeApp({
+// Your web app's Firebase configuration
+  apiKey: "AIzaSyCPNj3pL1DrmcmlYOZ3OgcSxOHi5pbK-l0",
+  authDomain: "rifamc-40f07.firebaseapp.com",
+  databaseURL: "https://rifamc-40f07.firebaseio.com",
+  projectId: "rifamc-40f07",
+  storageBucket: "rifamc-40f07.appspot.com",
+  messagingSenderId: "56794161319",
+  appId: "1:56794161319:web:a143a683552436b33dbb82"
+})
+
+let db = firebaseApp.firestore();
+
+export { db }
