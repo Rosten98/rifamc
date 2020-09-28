6ae7ed270779317788663273292be34adc0f61c5
Gerardo <gerodel98@gmail.com>
2020-09-28 18:27:10 -0500

    Added call to firebase

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,17 +1,52 @@
-import React from 'react'
-import Header from './Header'
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Header from "./Header";
 
 const Admin = () => {
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+    db.collection("contacts")
+      .orderBy("date", "asc")
+      .get()
+      .then((querySnapshot) => {
+        let docs = [];
+        querySnapshot.forEach((doc) => {
+          // console.log(doc.id, " => ", doc.data().number, doc.data().selected);
+          docs.push({
+            firstName: doc.data().firstName,
+            lastName: doc.data().lastName,
+            phone: doc.data().phone,
+            localPhone: doc.data().localPhone,
+            mail: doc.data().mail,
+            imageUrl: doc.data().imageAsUrl,
+            date: doc.data().date,
+            group: doc.data().group,
+            selectedNumbers: doc.data().selectedNumbers,
+            paymentType: doc.data().paymentType,
+            ticketNumber: doc.data().ticketNumber,
+            payValue: doc.data().payValue
+          });
+        });
+        setParticipants(docs);
+      })
+      .catch(function (error) {
+        console.log("Error getting documents: ", error);
+      });
+  }, []);
+
+  console.log(participants)
   return (
     <div className="page">
       <main>
-        <Header/>
+        <Header />
         <section>
           <h3>Participantes registrados</h3>
           <div className="admin">
             <table>
               <thead>
                 <tr>
+                  <th>Fecha de registro</th>
                   <th>Nombre</th>
                   <th>Apellido</th>
                   <th>Celular</th>
@@ -27,6 +62,7 @@ const Admin = () => {
               </thead>
               <tbody>
                 <tr>
+                  <td>01/02/2020</td>
                   <td>Jill</td>
                   <td>Smith</td>
                   <td>5050505050</td>
@@ -36,10 +72,18 @@ const Admin = () => {
                   <td>Transferencia</td>
                   <td>1234567890</td>
                   <td>$123</td>
-                  <td><ul><li>1</li><li>2</li></ul></td>
-                  <td><button className="verBtn">Ver imagen</button></td>
+                  <td>
+                    <ul>
+                      <li>1</li>
+                      <li>2</li>
+                    </ul>
+                  </td>
+                  <td>
+                    <button className="verBtn">Ver imagen</button>
+                  </td>
                 </tr>
                 <tr>
+                  <td>01/02/2020</td>
                   <td>Jill</td>
                   <td>Smith</td>
                   <td>5050505050</td>
@@ -49,8 +93,15 @@ const Admin = () => {
                   <td>Transferencia</td>
                   <td>1234567890</td>
                   <td>$123</td>
-                  <td><ul><li>1</li><li>2</li></ul></td>
-                  <td><button className="verBtn">Ver imagen</button></td>
+                  <td>
+                    <ul>
+                      <li>1</li>
+                      <li>2</li>
+                    </ul>
+                  </td>
+                  <td>
+                    <button className="verBtn">Ver imagen</button>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -58,7 +109,7 @@ const Admin = () => {
         </section>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
