09f0e1eae2f3e3b7dc2815d56abbddbad47ccb24
Gerardo Rodriguez <gerardo.rodriguezdelgado@nxp.com>
2021-09-06 20:00:36 -0500

    Change logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <img src={require("../assets/full-ogo.jpg")} className="logo" alt="" />
-      <img src={require("../assets/title.jpeg")} className="logo" alt="" />
+      <img src={require("../assets/logo-rifa.jpg")} className="logo" alt="" />
       {/* <h1> Rifa navideña Miles Christi </h1> */}
     </header>
   )
